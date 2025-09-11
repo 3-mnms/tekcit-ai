cdb37ca3804c4e512be9d1e7c05105bab9defea2
CECHERI <24cecheri@gmail.com>
2025-09-11 12:33:06 +0900

    MNMS-630 Fix: 프론트에서 바로 FAST API 호출 가능하도록 CORS 수정

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ from fastapi import FastAPI
 from review_ai.server.routes import router as review_router
 from userchatbot_ai.server.routes import router as chatbot_router
 from activity_ai.server.routes import router as activity_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title = "TEKCIT-AI API",
     description="",
     version="1.0.0"
 )
+
+origins = [
+    "https://m.rookies-tekcit.com",
+    "http://localhost:3000"
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(review_router, prefix="/review")
 app.include_router(chatbot_router, prefix="/chat")
 app.include_router(activity_router, prefix="/activity")
